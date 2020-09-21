ab75e849ce891cb1aba0ed2af31763c203e5f983
JungTag <dbsdudxor121@naver.com>
2020-09-21 22:30:46 +0900

    롯데시네마 재개봉 프로젝트(여기서부터 브랜치 생성해주세요)

--- cinema/user/views.py
+++ cinema/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def initialize_DB(request):
+    return redirect('index')
+
+def index(request):
+    return render(request, 'index.html')
