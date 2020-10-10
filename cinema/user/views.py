fcd7dc375da2c97cddccd61b7f9306e36650a431
JungTag <dbsdudxor121@naver.com>
2020-10-10 19:33:18 +0900

    최종병합 (로그인/회원가입 약간 이상한 문제 / 초기화시 confirm창 띄워주기 아직 안만듦)

--- cinema/user/views.py
+++ cinema/user/views.py
@@ -265,10 +265,12 @@ def vote(request, movie_id): # 프론트에서 confirm 넣어줘야 함 -> yes�
     else: # 중복 투표 // alert있었으면 좋겠음
         return redirect(next)
 
-
 def detail(request, movie_id):
     selected_movie = Movie.objects.get(id=movie_id)
-    next = request.GET['next']
+    if 'next' in request.POST:
+        next = request.POST['next']
+    else:
+        next = None
     return render(request, 'detail.html', {'movie' : selected_movie, 'next' : next})
 
 def logout(request):
